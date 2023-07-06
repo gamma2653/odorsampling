c93e7da5c3d56348d7661bb15caeedf2959b539b
Christopher De Jesus <Christopher2653@gmail.com>
2023-07-06 18:04:21 -0400

    Added restructured cell code.

--- cells.py
+++ cells.py
@@ -6,47 +6,40 @@
 # Summer 2023
 
 from __future__ import annotations
+import builtins
+
+from collections import Counter
 
 import logging
-
 import config
 
 # Used for asserts
+if builtins.__debug__:
+    from numbers import Real
+
 
 # Type checking
 from typing import TYPE_CHECKING
-
 if TYPE_CHECKING:
     from typing import MutableMapping
     from RnO import Receptor
-    from numbers import Real
+    
 
 logger = logging.getLogger(__name__)
 config.default_log_setup(logger)
 
+cell_counter = Counter()
+"""
+Maps cell type to the number of cells of that type that have been created.
+"""
+# TODO: Add multithreading support
+def add_cell(cell_type: type, is_type=True) -> None:
+    """Increments the number of cells of the given type that have been created."""
+    cell_counter[cell_type if is_type else type(cell_type)] += 1
+
 # TODO: consider making these dataclasses
 
-# TODO: update numpy docstrings
-class Glom:
-    """
-    Represents a glomerulus cell that communicates with a single receptor.
-
-    Attributes
-    ----------
-    _id : int
-        Identifies the glomerulus
-    _activ : float
-        Between (0,1) - activation level of glomerulus
-    _loc : Tuple[float, float]
-        x,y coordinates of the glom on the surface of the Olfactory bulb
-    _dim : Tuple[float, float]
-        row x columns 
-    _conn : int
-        Number of mitral cells connected to
-    _recConn : dict
-        dict of connecting recs:weights
-    """
-
+class Cell:
     @property
     def id(self) -> int:
         return self._id
@@ -82,6 +75,34 @@ class Glom:
         # FIXME: ensure tuple
         assert len(value) == 2 and isinstance(value[0], Real), "Not a 2D list of numbers!"
         self._loc = tuple(value)
+    
+    def __init__(self, id_: int, activ: float, loc: tuple[Real, Real]) -> None:
+        """Initializes a cell with an id, activation level, and location."""
+        self.id = id_
+        self.activ = activ
+        self.loc = loc
+        add_cell(self.__class__)
+
+# TODO: update numpy docstrings
+class Glom(Cell):
+    """
+    Represents a glomerulus cell that communicates with a single receptor.
+
+    Attributes
+    ----------
+    _id : int
+        Identifies the glomerulus
+    _activ : float
+        Between (0,1) - activation level of glomerulus
+    _loc : Tuple[float, float]
+        x,y coordinates of the glom on the surface of the Olfactory bulb
+    _dim : Tuple[float, float]
+        row x columns 
+    _conn : int
+        Number of mitral cells connected to
+    _recConn : dict
+        dict of connecting recs:weights
+    """
 
     @property
     def dim(self) -> tuple[int]:
@@ -118,9 +139,7 @@ class Glom:
 
     def __init__(self, id_, activ=0.0, loc=(0,0), dim=(0,0), conn=0):
         """Initializes Glom object"""
-        self._id: int = id_
-        self._activ: float = activ
-        self._loc: tuple[float, float] = loc
+        super().__init__(id_, activ, loc)
         self._dim: tuple[float, float] = dim
         self._conn: int = conn
         # TODO: Took me some time to track down this type. Would be a circular import,
@@ -133,7 +152,7 @@ class Glom:
         return f"Id: {self.id} activ: {self.activ}"
 
 
-class Mitral:
+class Mitral(Cell):
     """Represents a mitral cell that samples from glomeruli.
     Attributes
     ----------
@@ -147,41 +166,6 @@ class Mitral:
         where the keys are glom and the values are weights
     
     """
-    @property
-    def id(self) -> int:
-        """Returns ID of mitral"""
-        return self._id
-
-    @id.setter
-    def id(self, value: int) -> None:
-        """Sets value to id.
-        Precondition: value is an integer"""
-        self._id = int(value)
-
-    @property
-    def activ(self) -> float:
-        """Returns activation level of Mitral."""
-        return self._activ
-
-    @activ.setter
-    def activ(self, value: float) -> None:
-        """Rounds value and sets it to activation level.
-        Precondition: Value is a float between 0 and 1."""
-        N_DIGITS = 5
-        assert value <= 1 and value >= 0, "Not between 0 and 1"
-        value = round(float(value), N_DIGITS)
-        
-    @property
-    def loc(self) -> tuple[Real, Real]:
-        """Returns location of mitral cell"""
-        return self._loc
-
-    @loc.setter
-    def loc(self, value: tuple[Real, Real]) -> None:
-        """Sets value to loc.
-        Precondition: value is a 2D list of numbers"""
-        assert len(value) == 2 and isinstance(value[0], Real), "Not a length: 2 tuple of numbers!"
-        self._loc = tuple(value)
         
     @property
     def glom(self) -> MutableMapping[Glom, float]:
@@ -196,9 +180,7 @@ class Mitral:
 
     def __init__(self, id_, activ=0.0, loc: tuple[float, float]=(0.0,0.0), glom=None):
         """Initializes a Mitral cell"""
-        self.id = id_
-        self.activ = activ
-        self.loc = loc
+        super().__init__(id_, activ, loc)
         self.glom = {} if glom is None else glom
 
     def __str__(self):
@@ -207,4 +189,3 @@ class Mitral:
         # *********************** From Python 3.6 onwards, the standard dict type maintains insertion order by default *****************************
         gstring = self.glom.keys()
         return f"Mitral ID: {self.id} Mitral Activ: {self.activ} Glom: {gstring}"
-
